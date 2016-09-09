5a320a7f1b7fb36ceb551984b972e450d5544578
Sarah Veness <sarahhveness@gmail.com>
2016-09-09 20:01:02 +0000

    created private function

---
+++ my-importer.js
@@ -0,0 +1,5 @@
+var myModule = require("./my-module.js");
+
+console.log(myModule.public());
+
+// console.log(myModule.private);
---
+++ my-module.js
@@ -0,0 +1,11 @@
+module.exports = {
+ PHI: 1.618,
+
+ public: function() {
+   console.log("HI");
+   function private() {
+     console.log("The Fourth Kind wasn't real.");
+   }
+   private();
+ }
+};
